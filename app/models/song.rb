ef9940ff076d5f8d0867454d65076c06c61acbda
Indre Bakeer <indre.bakeer@gmail.com>
2019-08-27 17:36:13 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,6 +1,9 @@
 class Song < ApplicationRecord
-  validates_presence_of :title, :artist_name
-  validates_uniqueness_of :title, scope: [:artist_name, :release_year]
+  validates :title, presence: true
+  validates :artist_name, presence: true
+  validates :title, uniqueness: true, scope: [:artist_name, :release_year]
+#  validates_presence_of :title, :artist_name
+#  validates_uniqueness_of :title, scope: [:artist_name, :release_year]
   validates_inclusion_of :released, in: [true, false]
   validates_presence_of :release_year, if: :released
   validate :valid_release_date?
